ec46392076021a2721445459692ceba8ea4eaf98
Wim Pool <wim.pool@ihomer.nl>
2016-01-26 17:02:19 +0100

    reCenter map on center property change

--- addon/components/mapbox-map.js
+++ addon/components/mapbox-map.js
@@ -2,11 +2,22 @@ import Ember from 'ember';
 import layout from '../templates/components/mapbox-map';
 
 export default Ember.Component.extend({
+  classNameBindings: ['reCenter'],
   layout: layout,
   divId: 'map',
-
   mapId: null,
-
+  
+  reCenter: Ember.computed('center', function() {
+    var map = this.get('map');
+    var center = this.get('center');
+    if (typeof(map) != 'undefined' && center != null) {
+      map.setView(center, this.get('zoom'));
+      return true;
+    } else {
+      return false;
+    }
+  }),
+  
   setup: Ember.on('didInsertElement', function() {
     Ember.run.scheduleOnce('afterRender', this, function () {
       let map = L.mapbox.map(this.get('divId'), this.get('mapId'));
