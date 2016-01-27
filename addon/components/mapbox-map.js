4d0cc8c0f80703b03243fac2f2180f8efceec417
Wim Pool <wim.pool@ihomer.nl>
2016-01-27 13:21:27 +0100

    Center and Resize added
    When the properties center and resize change the map is centered and resized. The center property already existed, for the resize property I use a number that is incremented everytime I want a resize of the map.

--- addon/components/mapbox-map.js
+++ addon/components/mapbox-map.js
@@ -2,10 +2,31 @@ import Ember from 'ember';
 import layout from '../templates/components/mapbox-map';
 
 export default Ember.Component.extend({
+  classNameBindings: ['centered', 'resized'],
   layout: layout,
   divId: 'map',
-
+  
   mapId: null,
+  
+  centered: Ember.computed('center', function() {
+    var map = this.get('map');
+    var center = this.get('center');
+    if (typeof(map) != 'undefined' && center != null) {
+      map.setView(center, this.get('zoom'));
+      return true;
+    } else {
+      return false;
+    }
+  }),
+  resized: Ember.computed('resize', function() {
+    var map = this.get('map');
+    if (typeof(map) != 'undefined') {
+      map.invalidateSize();
+      return true;
+    } else {
+      return false;
+    }
+  }),
 
   setup: Ember.on('didInsertElement', function() {
     Ember.run.scheduleOnce('afterRender', this, function () {
